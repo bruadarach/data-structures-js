b113f3ce62ba4da9284e34018e241a7a7172edde
Sujeong Ji <suji.sujeongji@gmail.com>
2022-12-09 01:45:19 +0900

    07. linked list

---
+++ 07-linked-list/02-linked-list-prepend.js
@@ -0,0 +1,48 @@
+// prepend
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  prepend(value) {
+    const node = new Node(value);
+
+    if (this.isEmpty()) {
+      // no list
+      this.head = node;
+    } else {
+      // list existed
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+}
+
+// class
+const list = new LinkedList();
+console.log("List is empty? ", list.isEmpty()); // true
+console.log("List size ", list.getSize()); // 0
+
+// prepend
+list.prepend(10);
+list.prepend(20);
+list.prepend(30);
+
+console.log(list);
---
+++ 07-linked-list/03-linked-list-print.js
@@ -0,0 +1,73 @@
+// prepend
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  // add front
+  prepend(value) {
+    const node = new Node(value);
+
+    if (this.isEmpty()) {
+      // no list
+      this.head = node;
+    } else {
+      // list existed
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is empty");
+    } else {
+      let curr = this.head;
+      let listValues = "";
+
+      while (curr) {
+        listValues += `${curr.value} `;
+        curr = curr.next;
+      }
+
+      console.log(listValues);
+    }
+  }
+}
+
+// class
+const list = new LinkedList();
+console.log("List is empty? ", list.isEmpty()); // true
+console.log("List size ", list.getSize()); // 0
+list.print(); // List is empty
+
+// prepend
+list.prepend(10);
+list.print(); // 10
+list.prepend(20);
+list.prepend(30);
+list.print(); // 30 20 10
+
+// print
+console.log(list);
+// LinkedList {
+//     head: Node { value: 30, next: Node { value: 20, next: [Node] } },
+//     size: 3
+//   }
