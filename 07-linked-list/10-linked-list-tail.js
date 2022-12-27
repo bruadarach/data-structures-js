73496f47147c2154c4a2dc286261215856f79809
Sujeong Ji <suji.sujeongji@gmail.com>
2022-12-27 23:50:33 +0900

    07. linked list

--- 07-linked-list/10-linked-list-tail.js
+++ 07-linked-list/10-linked-list-tail.js
@@ -37,13 +37,63 @@ class LinkedList {
     }
   }
 
-  prepend(value) {}
+  // constant time complexity
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
 
-  append(value) {}
+  // constant time complexity
+  append(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.size++;
+  }
 
-  removeFromFront() {}
+  // constant time complexity
+  removeFromFront() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    this.size--;
+    return value;
+  }
 
-  removeFromEnd() {}
+  // linear time complexity
+  removeFromEnd() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const value = this.tail.value;
+    if (this.size === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let prev = this.head;
+      while (prev.next !== this.tail) {
+        prev = prev.next;
+      }
+      prev.next = null;
+      this.tail = prev;
+    }
+    this.size--;
+    return value;
+  }
 }
 
 // class
@@ -51,3 +101,18 @@ const list = new LinkedList();
 console.log("List is empty? ", list.isEmpty()); // true
 console.log("List size ", list.getSize()); // 0
 list.print(); // List is empty
+
+list.append(1);
+list.append(2);
+list.append(3);
+list.print(); // 1 2 3
+
+list.prepend(0);
+list.print(); // 0 1 2 3
+console.log(list.getSize()); // 4
+
+list.removeFromFront();
+list.print(); // 1 2 3
+
+list.removeFromEnd();
+list.print(); // 1 2
