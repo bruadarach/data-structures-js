3fa8e2fa1acf67b565b6229df2e580fc18fc7e1f
Sujeong Ji <suji.sujeongji@gmail.com>
2022-12-28 23:34:11 +0900

    07. linked list

---
+++ 07-linked-list/11-linked-list-stack.js
@@ -0,0 +1,18 @@
+const LinkedList = required("./11-linked-list.js");
+
+class LinkedListStack {
+  constructor() {
+    this.list = new LinkedList();
+  }
+  push(value) {}
+
+  pop() {}
+
+  peek() {}
+
+  isEmpty() {}
+
+  getSize() {}
+
+  print() {}
+}
---
+++ 07-linked-list/11-linked-list.js
@@ -0,0 +1,101 @@
+// Stack: last in first out
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null; // add
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is empty");
+      // list exists => travel nodes
+    } else {
+      let curr = this.head;
+      let listValues = "";
+
+      // curr !== null
+      while (curr) {
+        listValues += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log(listValues);
+    }
+  }
+
+  // constant time complexity
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+
+  // constant time complexity
+  append(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.size++;
+  }
+
+  // constant time complexity
+  removeFromFront() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    this.size--;
+    return value;
+  }
+
+  // linear time complexity
+  removeFromEnd() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const value = this.tail.value;
+    if (this.size === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let prev = this.head;
+      while (prev.next !== this.tail) {
+        prev = prev.next;
+      }
+      prev.next = null;
+      this.tail = prev;
+    }
+    this.size--;
+    return value;
+  }
+}
+
+module.exports = LinkedList;
