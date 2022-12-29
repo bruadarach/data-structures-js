d32b76775379ccdfd6a87918eddf7f6834afdd8a
Sujeong Ji <suji.sujeongji@gmail.com>
2022-12-29 23:55:28 +0900

    07. linked list

--- 07-linked-list/11-linked-list-stack.js
+++ 07-linked-list/11-linked-list-stack.js
@@ -1,18 +1,43 @@
-const LinkedList = required("./11-linked-list.js");
+const LinkedList = require("./11-linked-list.js");
 
 class LinkedListStack {
   constructor() {
     this.list = new LinkedList();
   }
-  push(value) {}
+  push(value) {
+    this.list.prepend(value);
+  }
 
-  pop() {}
+  pop() {
+    return this.list.removeFromFront();
+  }
 
-  peek() {}
+  peek() {
+    return this.list.head.value;
+  }
 
-  isEmpty() {}
+  isEmpty() {
+    return this.list.isEmpty();
+  }
 
-  getSize() {}
+  getSize() {
+    return this.list.getSize();
+  }
 
-  print() {}
+  print() {
+    return this.list.print();
+  }
 }
+
+// instantiate a class
+const stack = new LinkedListStack();
+console.log(stack.isEmpty()); // true
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+console.log(stack.getSize()); // 3
+stack.print(); // 30 20 10
+
+console.log(stack.pop()); // 30
+console.log(stack.peek()); // 20
