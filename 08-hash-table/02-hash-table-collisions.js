b7792a9f19a1dce4a43cefb8b7c50738e610e9f2
Sujeong Ji <suji.sujeongji@gmail.com>
2023-01-05 23:16:55 +0900

    08. hash table

---
+++ 08-hash-table/02-hash-table-collisions.js
@@ -0,0 +1,72 @@
+class HashTable {
+  constructor(size) {
+    this.table = new Array(size);
+    this.size = size;
+  }
+
+  hash(key) {
+    let total = 0;
+    for (let i = 0; i < key.length; i++) {
+      total += key.charCodeAt(i);
+    }
+    return total % this.size;
+  }
+
+  set(key, value) {
+    //=> changed
+    const index = this.hash(key);
+    //this.table[index] = value;
+    const bucket = this.table[index];
+    if (!bucket) {
+      bucket = [[key, value]];
+    } else {
+      const sameKeyItem = bucket.find((iten) => item[0] === key);
+      if (sameKeyItem) {
+        sameKeyItem[1] = value;
+      } else {
+        bucket.push([key, value]);
+      }
+    }
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    return this.table[index];
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    this.table[index] = undefined;
+  }
+
+  display() {
+    for (let i = 0; i < this.table.length; i++) {
+      if (this.table[i]) {
+        console.log(i, this.table[i]);
+      }
+    }
+  }
+}
+
+const table = new HashTable(50);
+table.display();
+
+table.set("name", "Sujeong Ji");
+table.set("pet", "Lucky");
+table.display();
+// 17 Sujeong Ji
+// 29 Lucky
+
+console.log(table.get("name")); // Sujeong Ji
+
+table.remove("name");
+table.display();
+// 29 Lucky
+
+table.set("pet", "Deoqua");
+table.display();
+// 29 Deoqua
+
+table.set("tep", "collision happened because of charCodeAt!");
+table.display();
+// 29 collision happened because of charCodeAt!
