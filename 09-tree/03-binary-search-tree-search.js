078eda4057af2c8a70e3f61cb0b002b475f75345
Sujeong Ji <suji.sujeongji@gmail.com>
2023-01-12 22:47:50 +0900

    09. tree

---
+++ 09-tree/03-binary-search-tree-search.js
@@ -0,0 +1,67 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  isEmpty() {
+    return this.root === null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(root, newNode) {
+    if (newNode.value < root.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
+
+  search(root, value) {
+    if (!root) {
+      return false;
+    } else {
+      if (root.value === value) {
+        return true;
+      } else if (value < root.value) {
+        return this.search(root.left, value);
+      } else {
+        return this.search(root.right, value);
+      }
+    }
+  }
+}
+
+const bst = new BinarySearchTree();
+console.log("Tree is empty?", bst.isEmpty()); // true
+
+bst.insert(10);
+bst.insert(20);
+bst.insert(30);
+
+console.log(bst.search(bst.root, 10)); // true
+console.log(bst.search(bst.root, 20)); // true
+console.log(bst.search(bst.root, 50)); // false
